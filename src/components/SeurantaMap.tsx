85d802920874d9bb5287e0b2300d3502463df9f6
xbexbex <petja.valkama@gmail.com>
2019-12-06 11:31:30 +0200

    Use MML background map

--- src/components/SeurantaMap.tsx
+++ src/components/SeurantaMap.tsx
@@ -1,27 +1,27 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
-import "leaflet/dist/leaflet.css";
+import 'leaflet/dist/leaflet.css';
 
 import { Theme } from '../styles';
 
-const position = [51.505, -0.09];
+const position = [60.192059, 24.945831];
 
 const SeurantaMap: React.FC<{}> = ({}) => {
   return (
-      <MapContainer center={position} zoom={13}>
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        />
-        <Marker position={position}>
-          <Popup>
-            A pretty CSS3 popup.
-            <br />
-            Easily customizable.
-          </Popup>
-        </Marker>
-      </MapContainer>
+    <MapContainer center={position} zoom={14}>
+      <TileLayer
+        url="http://tiles.kartat.kapsi.fi/taustakartta/{z}/{x}/{y}.png"
+        attribution='&copy; Karttamateriaali <a href="http://www.maanmittauslaitos.fi/avoindata">Maanmittauslaitos</a>'
+      />
+      <Marker position={position}>
+        <Popup>
+          A pretty CSS3 popup.
+          <br />
+          Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
   );
 };
 
@@ -29,6 +29,5 @@ const MapContainer: any = styled(Map)`
   display: flex;
   flex: 1;
 `;
-
 
 export default SeurantaMap;
