e12643df71e4e3e2e7e020df067ff6939a1214d1
xbexbex <petja.valkama@gmail.com>
2019-12-09 13:34:23 +0200

    Update marker styling

--- src/components/ObservationPoint.tsx
+++ src/components/ObservationPoint.tsx
@@ -5,49 +5,123 @@ import { Marker, Popup, Circle } from 'react-leaflet';
 
 import { Theme } from '../styles';
 import { ObsPointData } from '../utils/types';
+import { unstable_renderSubtreeIntoContainer, render } from 'react-dom';
 
 interface Props {
   obs: ObsPointData;
 }
 
+const NO_COLOR = 'gray';
+const COLORS = ['#0eb51f', '#afde16', '#dce312', '#de8716', '#b5110e'];
+const LEVEL_OF_NEED = [
+  'ei tarvetta',
+  'pieni',
+  'keskisuuri',
+  'suurehko',
+  'suuri',
+];
+
 const ObservationPoint: React.FC<Props> = (props: Props) => {
+  const [renderSettings, setRenderSettings] = useState<any>(null);
   const position: number[] = [props.obs.lat, props.obs.long];
+
+  useEffect(() => {
+    let color: string;
+    let size: number;
+    const settings: any = {};
+
+    if (
+      props.obs.Sd !== undefined &&
+      props.obs.Smax !== undefined &&
+      props.obs.Smin !== undefined
+    ) {
+      const index = Math.max(Math.min(Math.floor(props.obs.Sd / 20 + 1), 4), 0);
+      color = COLORS[index];
+      size = 40 + index * 4;
+      settings.levelOfNeed = LEVEL_OF_NEED[index];
+    } else {
+      size = 40;
+      color = NO_COLOR;
+    }
+
+    const icon = L.divIcon({
+      className: 'pin',
+      iconAnchor: [size / 2, size],
+      labelAnchor: [-6, 0],
+      popupAnchor: [0, (size - 4) * -1],
+      html: `<svg xmlns="http://www.w3.org/2000/svg" 
+        width="${size}" 
+        height="${size}" 
+        viewBox="0 0 24 24" 
+        fill="${color}"
+        fill-opacity="0.8" 
+        stroke="black" 
+        stroke-width="1.5" stroke-linecap="round" 
+        stroke-linejoin="round" 
+        class="feather feather-map-pin">
+        <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
+        <circle cx="12" cy="10" r="3"></circle></svg>`,
+    });
+
+    settings.color = color;
+    settings.icon = icon;
+    setRenderSettings(settings);
+  }, []);
 
   return (
     <>
-      <Point position={position} icon={icon}>
-        <Tooltip>{props.obs.serviceName}</Tooltip>
-      </Point>
-      <Circle center={position} radius={10} color={'#3388ff'} />
+      {renderSettings && (
+        <>
+          (
+          <Point position={position} icon={renderSettings.icon}>
+            <Tooltip>
+              <Header>
+                {props.obs.serviceName} <br />
+              </Header>
+              <hr/>
+              {renderSettings.levelOfNeed && (
+                <Field><b>Havainnon tarve:</b>{' ' + renderSettings.levelOfNeed}</Field>
+              )}
+              {props.obs.serviceId && (
+                <Field><b>Ilmoituspalvelu:</b>{' ' + props.obs.serviceId}</Field>
+              )}
+            </Tooltip>
+          </Point>
+          )
+          {props.obs.radius && (
+            <Circle
+              center={position}
+              radius={props.obs.radius}
+              color={'black'}
+              fillColor={renderSettings.color}
+              weight={1}
+              opacity={0.6}
+              fillOpacity={0.65}
+            />
+          )}
+        </>
+      )}
     </>
   );
 };
 
-const Point: any = styled(Marker)`
-  background-color: red;
-  color: red;
+const Point: any = styled(Marker)``;
+
+const Tooltip: any = styled(Popup)`
+  flex: 1;
+  flex-direction: column;
+
 `;
 
-const myCustomColour = '#583470';
+const Header: any = styled.p`
+  font-size: 1rem;
+  margin: 0 !important;
+`;
 
-const MarkerHtml = `
-  background-color: ${myCustomColour};
-  width: 30px;
-  height: 30px;
-  display: inline-block;
-  position: relative;
-  border-radius: 3rem 3rem 0;
-  transform: rotate(45deg);
-  border: 1px solid #FFFFFF`;
-
-const icon = L.divIcon({
-  className: 'my-custom-pin',
-  iconAnchor: [20, 40],
-  labelAnchor: [-6, 0],
-  popupAnchor: [0, -36],
-  html: `<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="red" fill-opacity="0.7" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-map-pin"><path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path><circle cx="12" cy="10" r="3"></circle></svg>`,
-});
-
-const Tooltip: any = styled(Popup)``;
+const Field: any = styled.p`
+  font-size: 0.8rem;
+  line-height: 1rem;
+  margin: 8px 0 0 0 !important;
+`;
 
 export default ObservationPoint;
